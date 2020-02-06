9de2d262614152f3cc97394918d402ed0e52c382
jonatancoster <jonatan.coster@gmail.com>
2020-02-06 11:53:46 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String args[]) {
+      Sort.sort();
+      Search.search();
+    }
+}
+
+class Sort {
+    static void sort() {
+        System.out.println("sort");
+    }
+}
+
+class Search {
+    static void search() {
+        System.out.println("search");
+    }
+}
