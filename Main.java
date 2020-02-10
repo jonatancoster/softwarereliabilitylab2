4cf7661a944058a0811c06d8e397ccb419db1234
jonatancoster <jonatan.coster@gmail.com>
2020-02-10 13:18:15 +0100

    Added testing framework
    The testing framework reads test cases from a text file. Each row corresponds to one test case. On each row, all elements except the last one are used to construct the array, and the last element is used as the key. Note that the first row is skipped.

--- Main.java
+++ Main.java
@@ -1,16 +1,42 @@
 import java.util.Arrays;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]) {
-      int[] unsorted = new int[]{5,9,3,7,5,1,0,9,8};
-      System.out.println(Arrays.toString(unsorted));
+
+      try{
+        File testCases = new File("pairwise.txt");
+        Scanner scanner = new Scanner(testCases);
+        scanner.nextLine();
+        while (scanner.hasNextLine()) {
+          // Read a test case.
+          String[] row = scanner.nextLine().split("\t");
+          // The key is the lase element on the line.
+          int key = Integer.parseInt(row[row.length - 1]);
+          // The array is made of all the elements except the last one.
+          String[] modifiedRow = Arrays.copyOf(row, row.length - 1);
+          int[] array = stringArrayToIntArray(modifiedRow);
+          System.out.println(Arrays.toString(array));
+        }
+        scanner.close();
+      } catch(FileNotFoundException e) {
+        System.out.println("An error occurred.");
+        e.printStackTrace();
+      }
       
-      int key = 5;
-      
-      int member = ArrayFunctions.isMemberOfUnsortedArray(unsorted, key);
-      
-      System.out.println(member);
-      
-      System.out.println(Arrays.toString(unsorted));
+    }
+
+
+
+
+
+    private static int[] stringArrayToIntArray(String[] stringArray) {
+      int[] intArray = new int[stringArray.length];   
+      for(int i = 0; i < stringArray.length; i++) {
+        intArray[i] = Integer.parseInt(stringArray[i]);
+      }
+      return intArray;
     }
 }
